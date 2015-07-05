731d4aab921117aed7adc4cefab6463fcb899b49
atlantis <attilaatlantis@yahoo.de>
2015-07-06 01:10:59 +0200

    alle male aquamala

--- js/game.js
+++ js/game.js
@@ -3,15 +3,29 @@ window.onload = function(){
 };
 
 function meteorstorm(){
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload:preload, create:create, update:update});
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '',{preload:preload, create:create, update:update});
 
     function preload(){
+        game.load.image('background', 'assets/crappyStarfield.png');
+        game.load.image('meteor', 'assets/meteor.png');
+        game.load.image('spaceship', 'assets/spaceship.png');
 
+        game.load.audio('music', 'assets/music.mp3');
+        game.load.audio('crash', 'assets/crash.ogg');
+
+        game.load.spritesheet('animation', 'assets/animation.png', 90, 95);
     }
     function create(){
 
+        var music = new Phaser.Sound(game, 'music', 1, true);
+        music.autoplay = true;
+        music.loop = true;
+        music.play();
+
+        game.add.sprite(0,0, 'background');
+        game.add.sprite(game.width / 2, game.height / 2, 'spaceship');
     }
     function update(){
-        
+
     }
 }
